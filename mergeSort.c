54a616a5dd197445d75a9958537b07b0ea61d4c0
Mr. Techno Star <118105307+techstaraj@users.noreply.github.com>
2024-04-25 11:18:45 +0530

    Create mergeSort.c

---
+++ mergeSort.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+void merge(int *arr,int l,int mid,int r){
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+    int a[n1],b[n2];
+
+    for (int i = 0; i < n1; i++)
+    {
+        a[i] = arr[l+i];
+    }
+
+    for (int i = 0; i < n2; i++)
+    {
+        b[i] = arr[mid+i+1];
+    }
+
+    int i = 0,j = 0,k = l;
+    while(i < n1 && j < n2){
+        if(a[i] < b[j]){
+            arr[k] = a[i];
+            k++; i++;
+        }else{
+            arr[k] = b[j];
+            k++; j++;
+        }
+    }
+    while(i < n1){
+        arr[k] = a[i];
+        k++; i++;
+    }
+    while(j < n2){
+        arr[k] = b[j];
+        k++; j++;
+    }
+    
+}
+
+void mergeSort(int *arr,int l,int r){
+    if(l < r){
+        int  mid = (r+l)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
+    }
+}
+
+int main(){
+
+    int arr[20];
+    int n,i;
+    
+    printf("Enter No. of Elements(<20) : ");
+    scanf("%d",&n);
+
+    printf("Enter %d Elements : ",n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    mergeSort(arr,0,8);
+
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    printf("\n");
+
+    return 0;
+}
